d8540cfae29c2f1967fa0287bb6e90d120699a9b
Helbert Pinto <helbert.tec.info@gmail.com>
2021-04-15 13:03:05 -0300

    ajustando radios

--- src/components/exams/Form.vue
+++ src/components/exams/Form.vue
@@ -5,17 +5,22 @@
         <p>Idade:{{data.patientAge}}</p>
     </div>
     <div class="items">
-        <el-radio v-model="radio" label="1">Pós operatório: Miopia/RK</el-radio>
-        <el-radio v-model="radio" label="4">Ceratocone 1</el-radio>
-        <el-radio v-model="radio" label="7">Normal</el-radio>
-        <el-radio v-model="radio" label="2">Pós operatório: Hipermetropia</el-radio>
-        <el-radio v-model="radio" label="5">Ceratocone 2</el-radio>  
-        <el-radio v-model="radio" label="8">Pelúcida</el-radio>
-        <el-radio v-model="radio" label="3">Pós operatório: Transp Córnea</el-radio>
-        <el-radio v-model="radio" label="6">Ceratocone 3</el-radio>
-        <el-radio v-model="radio" label="9">Outros</el-radio>
-        <el-radio v-model="radio" label="10">Deformação corneana por lente de contato</el-radio>
-        <div></div>
+        <div class="items-radio">
+            <el-radio v-model="radio" label="1">Pós operatório: Miopia/RK</el-radio>
+            <el-radio v-model="radio" label="2">Pós operatório: Hipermetropia</el-radio>
+            <el-radio v-model="radio" label="3">Pós operatório: Transp Córnea</el-radio>   
+            <el-radio v-model="radio" label="4">Deformação corneana por lente de contato</el-radio>
+        </div>
+        <div class="items-radio">
+            <el-radio v-model="radio" label="5">Ceratocone 1</el-radio>
+            <el-radio v-model="radio" label="6">Ceratocone 2</el-radio>  
+            <el-radio v-model="radio" label="7">Ceratocone 3</el-radio>
+        </div>
+        <div class="items-radio">
+            <el-radio v-model="radio" label="8">Normal</el-radio>
+            <el-radio v-model="radio" label="9">Pelúcida</el-radio>
+            <el-radio v-model="radio" label="10">Outros</el-radio>
+        </div>
     </div>
     <div class="submit">
         <el-button type="primary" @click="$emit('nextExam',radio)">Próximo</el-button>
@@ -34,7 +39,7 @@
         },
         data() {
             return {
-                radio: '7'
+                radio: '10'
             }
         },
         computed: {
@@ -47,13 +52,21 @@
 
 <style scoped>
     .items{
+        margin-left: 10px;
         width: 80%;
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
         grid-row-gap: 10px;
         align-items: center;
     }
+    .items-radio{
+        display: grid;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
+        text-align: initial;
+        grid-row-gap: 5px;
+    }
     .form{
+        margin: 5px;
         display: flex;
         box-sizing: border-box;
         background-color: #eee;
@@ -67,8 +80,9 @@
         justify-content: center;
     }
     .data{
-        padding-left: 20px;
+        padding-left: 5px;
         font-family: 'Quicksand';
         font-weight: bold;
+        text-align: initial;
     }
 </style>
