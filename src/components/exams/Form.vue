fee5dc62b54165c1a200f0ff6e1b88af00654860
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-08-31 10:16:55 -0300

    ajustando

--- src/components/exams/Form.vue
+++ src/components/exams/Form.vue
@@ -1,88 +1,87 @@
 <template>
-<div class="form">
+  <div class="form">
     <div class="data">
-        <p>Informações paciente</p>
-        <p>Idade:{{data.patientAge}}</p>
+      <p>Informações paciente:</p>
+      <p>Idade: {{ data.patientAge }}</p>
+      <p>Info 1: {{ data.info01 }}</p>
+      <p>Info 2: {{ data.info02 }}</p>
+      <p>Info 3: {{ data.info03 }}</p>
+      <p>Info 4: {{ data.info04 }}</p>
     </div>
-    <div class="items">
-        <div class="items-radio">
-            <el-radio v-model="radio" label="1">Pós operatório: Miopia/RK</el-radio>
-            <el-radio v-model="radio" label="2">Pós operatório: Hipermetropia</el-radio>
-            <el-radio v-model="radio" label="3">Pós operatório: Transp Córnea</el-radio>   
-            <el-radio v-model="radio" label="4">Deformação corneana por lente de contato</el-radio>
-        </div>
-        <div class="items-radio">
-            <el-radio v-model="radio" label="5">Ceratocone 1</el-radio>
-            <el-radio v-model="radio" label="6">Ceratocone 2</el-radio>  
-            <el-radio v-model="radio" label="7">Ceratocone 3</el-radio>
-        </div>
-        <div class="items-radio">
-            <el-radio v-model="radio" label="8">Normal</el-radio>
-            <el-radio v-model="radio" label="9">Pelúcida</el-radio>
-            <el-radio v-model="radio" label="10">Outros</el-radio>
-        </div>
+    <div class="data">
+      <div class="items-radio">
+        <el-radio v-model="radio" label="1">[1] Pós operatório: Miopia/RK</el-radio>
+        <el-radio v-model="radio" label="2">[2] Pós operatório: Hipermetropia</el-radio>
+        <el-radio v-model="radio" label="3">[3] Pós operatório: Transp Córnea</el-radio>
+        <el-radio v-model="radio" label="4">[4] Deformação corneana por lente de contato</el-radio>
+        <el-radio v-model="radio" label="5">[5] Ceratocone 1</el-radio>
+        <el-radio v-model="radio" label="6">[6] Ceratocone 2</el-radio>
+        <el-radio v-model="radio" label="7">[7] Ceratocone 3</el-radio>
+        <el-radio v-model="radio" label="8">[8] Normal</el-radio>
+        <el-radio v-model="radio" label="9">[9] Pelúcida</el-radio>
+        <el-radio v-model="radio" label="10">[0] Outros</el-radio>
+      </div>
     </div>
     <div class="submit">
-        <el-button type="primary" :disabled="Object.keys(data).length === 0" @click="$emit('nextExam',radio)">Próximo</el-button>
+      <el-button type="primary" :disabled="Object.keys(data).length === 0" @click="$emit('nextExam', radio)" >Próximo</el-button>
     </div>
-</div>
-
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            data: {
-                type: Object,
-                default:null
-            },
-        },
-        data() {
-            return {
-                radio: '10'
-            }
-        },
-        computed: {
-            newExamDate() {
-                return new Date(this.data.examDate) 
-            }
-        },
-    }
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      radio: "10",
+    };
+  },
+  computed: {
+    newExamDate() {
+      return new Date(this.data.examDate);
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .items{
-        margin-left: 10px;
-        width: 80%;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-row-gap: 10px;
-        align-items: center;
-    }
-    .items-radio{
-        display: grid;
-        grid-template-rows: 1fr 1fr 1fr 1fr;
-        text-align: initial;
-        grid-row-gap: 5px;
-    }
-    .form{
-        margin: 5px;
-        display: flex;
-        box-sizing: border-box;
-        background-color: #eee;
-        padding: 20px 0;
-        height: 100%;
-    }
-    .submit{
-        display: flex;
-        flex: 1;
-        align-items: center;
-        justify-content: center;
-    }
-    .data{
-        padding-left: 5px;
-        font-family: 'Quicksand';
-        font-weight: bold;
-        text-align: initial;
-    }
+.items {
+  margin-left: 10px;
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-gap: 10px;
+  align-items: center;
+}
+.items-radio {
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  text-align: initial;
+  grid-row-gap: 5px;
+}
+.form {
+  margin: 5px;
+  display: grid;
+  box-sizing: border-box;
+  background-color: #eee;
+  padding: 20px 0;
+  height: 100%;
+}
+.submit {
+  display: grid;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+}
+.data {
+  padding-left: 5px;
+  font-family: "Quicksand";
+  font-weight: bold;
+  text-align: initial;
+}
 </style>
--- src/views/Exam.vue
+++ src/views/Exam.vue
@@ -1,89 +1,107 @@
 <template>
-    <div class="container">
-        <div class="top">
-            <Img v-if="nextExamComputed" :src="nextExamComputed" />
-        </div>
-        <div>
-            <Form :data="this.nextExam? {patientAge: nextExam.patientAge, examDate: nextExam.examDate} : {}" @nextExam="nextExamMethod($event)"></Form>
-        </div>
+  <div class="container">
+    <div class="top">
+      <Img v-if="nextExamComputed" :src="nextExamComputed" />
     </div>
+    <div>
+      <Form
+        :data="
+          this.nextExam
+            ? {
+                patientAge: nextExam.patientAge,
+                info01: nextExam.info01,
+                info02: nextExam.info02,
+                info03: nextExam.info03,
+                info04: nextExam.info04
+              }
+            : {}
+        "
+        @nextExam="nextExamMethod($event)"
+      ></Form>
+    </div>
+  </div>
 </template>
 
 <script>
-import gql from 'graphql-tag'
-function _arrayBufferToBase64( buffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
-    }
-    return window.btoa( binary );
+import gql from "graphql-tag";
+function _arrayBufferToBase64(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
 }
 
 import Img from "@/components/exams/Img";
 import Form from "@/components/exams/Form";
-    export default {
-        components: {
-            Img,
-            Form,
-        },
-        computed:{
-            nextExamComputed(){
-                if(this.nextExam){
-                    return _arrayBufferToBase64( this.nextExam.file.data )
-                }
-                return null
-               
-            }
-        },
-        apollo:{
-            nextExam: {
-                query: gql`query Test($id: String!){
-                  nextExam(userId: $id){
-                    file,
-                    examDate
-                    patientAge
-                    examId
-                  }
-                }` ,
-                variables:{
-                  id: window.localStorage.getItem('userId')
-                }
-            },
-        },
-        created(){
-            /* this.$apollo.queries.nextExam.refetch() */
-        },
-        methods:{
-            async nextExamMethod(event) {
-                // Call to the graphql mutation
-                await this.$apollo.mutate({
-                  // Query
-                  mutation: gql`mutation ExamAns ($userId: String!,$examId: ID!,$answer: Int!) {
-                    examAns(userId: $userId,examId: $examId, answer: $answer)
-                  }`,
-                  // Parameters
-                  variables: {
-                    userId: window.localStorage.getItem('userId'),
-                    examId: this.nextExam.examId,
-                    answer: +event,
-                  },
-                })
-                this.$apollo.queries.nextExam.refetch()
-            }
+export default {
+  components: {
+    Img,
+    Form,
+  },
+  computed: {
+    nextExamComputed() {
+      if (this.nextExam) {
+        return _arrayBufferToBase64(this.nextExam.file.data);
+      }
+      return null;
+    },
+  },
+  apollo: {
+    nextExam: {
+      query: gql`
+        query Test($id: String!) {
+          nextExam(userId: $id) {
+            file
+            patientAge
+            info01
+            info02
+            info03
+            info04
+          }
         }
-    }
+      `,
+      variables: {
+        id: window.localStorage.getItem("userId"),
+      },
+    },
+  },
+  created() {
+    /* this.$apollo.queries.nextExam.refetch() */
+  },
+  methods: {
+    async nextExamMethod(event) {
+      // Call to the graphql mutation
+      await this.$apollo.mutate({
+        // Query
+        mutation: gql`
+          mutation ExamAns($userId: String!, $examId: ID!, $answer: Int!) {
+            examAns(userId: $userId, examId: $examId, answer: $answer)
+          }
+        `,
+        // Parameters
+        variables: {
+          userId: window.localStorage.getItem("userId"),
+          examId: this.nextExam.examId,
+          answer: +event,
+        },
+      });
+      this.$apollo.queries.nextExam.refetch();
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container{
-        height: calc(100vh - 45px);
-        display: grid;
-        grid-template-rows: 80% 20%;
-    }
-    .top{
-        display: flex;
-        flex-direction: column;
-    }
+.container {
+  height: calc(100vh - 5px);
+  display: grid;
+  grid-template-rows: 80% 20%;
+}
+.top {
+  display: flex;
+  flex-direction: column;
+}
 </style>
